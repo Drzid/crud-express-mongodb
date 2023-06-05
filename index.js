e8e8ec26817c785dd8857932633b76654e19a24c
Drzid <dzz@pop-os.localdomain>
2023-06-05 16:28:08 +0700

    add database connection

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 import express from "express";
+import mongoose from "mongoose";
+import {config} from "dotenv";
+config();
 
 const app = express();
 
-app.get ('/', (req,res) => {res.send('Welcome to Home')})
+const PORT = 3000;
 
-app.listen('3000',()=> {console.log("Server Running : Port 3000");});
+const mongoURI = process.env.MONGO_URI;
+
+mongoose.connect(mongoURI,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+});
+
+const db = mongoose.connection;
+db.on('error', (error)=>console.error(error));
+db.once('open', () => console.log('Database Connected'));
+
+app.get ('/', (req,res) => {
+    res.send('Welcome to Home')
+});
+
+    app.listen(PORT, () =>{
+        console.log(`Server running : ${PORT}`)
+    });
+
+
+
