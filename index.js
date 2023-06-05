ed76810f9eb842eea71f569770cfb56d5014c82c
Drzid <dzz@pop-os.localdomain>
2023-06-05 22:47:04 +0700

    added connection to mongodb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import route from "./routes/index.js"
 import {config} from "dotenv";
 config();
 
@@ -18,9 +19,7 @@ const db = mongoose.connection;
 db.on('error', (error)=>console.error(error));
 db.once('open', () => console.log('Database Connected'));
 
-app.get ('/', (req,res) => {
-    res.send('Welcome to Home')
-});
+app.use ('/api/product', route);
 
     app.listen(PORT, () =>{
         console.log(`Server running : ${PORT}`)
