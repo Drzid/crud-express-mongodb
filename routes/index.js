91d1144b44f7e635067a638304caebb8373f972e
Drzid <dzz@pop-os.localdomain>
2023-06-06 10:52:16 +0700

    added get by id

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { getProducts, saveProduct, updateProduct } from "../controllers/productController.js";
+import { getProductById, getProducts, saveProduct, updateProduct } from "../controllers/productController.js";
 
 const router = express.Router();
 
 router.get ('/', getProducts);
-router.get ('/:id');
+router.get ('/:id',getProductById);
 router.post ('/',saveProduct);
 router.put ('/:id', updateProduct);
 router.delete ('/');
