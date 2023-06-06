13427a8f596d50ce18ec960956be978d0b624ac2
Drzid <dzz@pop-os.localdomain>
2023-06-06 10:17:26 +0700

    added update data

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { getProducts, saveProduct } from "../controllers/productController.js";
+import { getProducts, saveProduct, updateProduct } from "../controllers/productController.js";
 
 const router = express.Router();
 
 router.get ('/', getProducts);
 router.get ('/:id');
 router.post ('/',saveProduct);
-router.put ('/');
+router.put ('/:id', updateProduct);
 router.delete ('/');
 
 export default router;
